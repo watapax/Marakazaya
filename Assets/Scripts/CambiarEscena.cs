97c71de0288cbcf434c9863a31ec413438c50693
Juan Correa <wuazopax@gmail.com>
2015-09-15 22:32:13 -0400

    Primer Build 15/09/2015

---
+++ Assets/Scripts/CambiarEscena.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambiarEscena : MonoBehaviour {
+
+
+	public void Cambiar(int indice)
+	{
+		Application.LoadLevel(indice);
+	}
+
+
+
+}
